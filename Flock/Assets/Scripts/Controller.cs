d714b238a86b0c8d17a094930084ad16fe68b43b
Elie <e.sarfati@rubika-edu.com>
2019-02-06 22:06:27 +0100

    Initial Commit

---
+++ Flock/Assets/Scripts/Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace ELS.Flock
+{
+    public class Controller : MonoBehaviour
+    {
+        [Serializable]
+        class Orientation
+        {
+            public float speed = 20f;
+
+            public void Update(Transform _origin, Transform _target)
+            {
+                Vector2 direction = _target.position - _origin.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+                Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                _origin.rotation = Quaternion.Slerp(_origin.rotation, rotation, speed * Time.deltaTime);
+            }
+        }
+
+        [Serializable]
+        class Target
+        {
+            public Transform transform;
+        }
+
+        [SerializeField] private Orientation orientation;
+
+        [SerializeField] private Transform target;
+
+        private Vector2 targetPos;
+
+
+        void Update()
+        {
+            orientation.Update(transform, target);
+        }
+
+        
+    }
+}
---
+++ Flock/Assets/Scripts/FOV.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ELS.Flock
+{
+    public class FOV : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> agents = new List<GameObject>();
+
+        [SerializeField] private List<GameObject> neighbors = new List<GameObject>();
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.tag == "Body" && !agents.Contains(collision.gameObject))
+            {
+                agents.Add(collision.gameObject);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if(agents.Contains(collision.gameObject))
+            {
+                agents.Remove(collision.gameObject);
+            }
+        }
+
+
+
+    }
+}
